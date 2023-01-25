9323c3cb21eb0b365247aa3423a8891f4fa0971e
Allen Ding <allen.ding@hotmail.com>
2023-01-24 18:55:32 -0800

    added post route for edit

--- express_server.js
+++ express_server.js
@@ -44,6 +44,11 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/urls/:id", (req,res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  res.redirect("/urls");
+});
+
 app.get("/u/:id", (req, res) => {
    const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);

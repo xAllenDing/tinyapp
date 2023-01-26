bf2404fc52bba335e1e1de374c926a4445a92a2d
Allen Ding <allen.ding@hotmail.com>
2023-01-25 16:58:35 -0800

    added form for username and a post route

--- express_server.js
+++ express_server.js
@@ -44,8 +44,14 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-app.post("/urls/:id", (req,res) => {
+app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
+  res.redirect("/urls");
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
   res.redirect("/urls");
 });
 
